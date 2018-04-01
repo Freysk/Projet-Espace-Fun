4350a34ddf91f165c896032011d9143968422ac9
freysk <frederic.defays@gmail.com>
2018-04-01 11:03:00 +0200

    Page nautique

--- src/js/main.js
+++ src/js/main.js
@@ -18,37 +18,25 @@ $burger.click(function(){
 $(document).ready(function() {
   if (window.matchMedia("(min-width: 800px)").matches) {
     var $swiperXtra = $('.moreActivities__pagination').add('.moreActivities__btn-next').add('.moreActivities__btn-prev');
-    
+
     $swiperContainer.removeClass('swiper-container')
     .children('.moreActivities__items-list').removeClass('swiper-wrapper')
     .children('.moreActivities__item').removeClass('swiper-slide');
     $swiperXtra.hide();
   } else {
-    /* L'affichage est inférieur à 600px de large */
   }
 })
 
-// //Changement du menu au Scroll
-// var $closeBtn = $('.navbar__link--btn-close');
-
-// $closeBtn.click(function () {
-//   $menu.toggleClass('modalMenu--inactive');
-//   if ($menu.hasClass('modalMenu--inactive')) {
-//     $(this).removeClass('fa-angle-right')
-//       .addClass('fa-angle-left')
-//       .prepend('<span class="bodytext"> Menu</span>');
-//   } else {
-//     $(this).removeClass('fa-angle-left').addClass('fa-angle-right').empty();
-//   }
-// })
-
-// $(window).scroll(function(){
-//   var $scrollTopPosition = $(window).scrollTop();
-//   var $windowHeight = $(window).height();
-//   if ($scrollTopPosition > $windowHeight) {
-//     $menu.addClass('modalMenu--inactive').css({'position':'fixed', 'right':'0'});
-//   } else {
-//     $menu.removeClass('modalMenu--inactive');
-//     $closeBtn.removeClass('fa-angle-left').addClass('fa-angle-right').empty();
-//   }
-// })
+$('.option__btn').click(function(){
+  
+  var $blockEl = $(this).parent('.option__block-el'),
+  $blockElSib = $blockEl.siblings('.option__block-el');
+  if ($blockEl.hasClass('.option__block-el-right')) {
+    $blockEl.removeClass('option__block-el-visible');
+    $blockElSib.addClass('option__block-el-visible');
+  } else {
+    console.log($(this).parent('.option__block-el'));
+    $blockEl.removeClass('option__block-el-visible');
+    $blockElSib.addClass('option__block-el-visible');
+  }
+})
